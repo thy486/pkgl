1392d8c17a2f528a16b8cc13ca024c6590a6495c
thy486 <83344148+thy486@users.noreply.github.com>
2021-07-06 01:49:30 +0800

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-set -e
-
-nohup /usr/bin/mysqld --user=root > /dev/null &
-
-if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
-  set -- node "$@"
-fi
-exec "$@"
+exec /usr/bin/mysqld --user=root --console
